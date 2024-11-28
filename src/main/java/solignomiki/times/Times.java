5a950b7beda7d1d7007eccea6ab2d301b4ddbb9c
solignomiki <ohmynuts@bk.ru>
2024-11-29 02:29:16 +0600

    added config for season length. for overworld extended only at the moment

--- src/main/java/solignomiki/times/Times.java
+++ src/main/java/solignomiki/times/Times.java
@@ -4,13 +4,18 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.ConfigHandler;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+
+import java.util.Properties;
 
 
 public class Times implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "times";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final ConfigHandler config;
+
     @Override
     public void onInitialize() {
         LOGGER.info("Times initialized.");
@@ -34,5 +39,11 @@ public class Times implements ModInitializer, GameStartEntrypoint, RecipeEntrypo
 	@Override
 	public void initNamespaces() {
 
+	}
+
+	static {
+		Properties props = new Properties();
+		props.setProperty("SeasonLength","28");
+		config = new ConfigHandler(MOD_ID, props);
 	}
 }
--- src/main/java/solignomiki/times/mixin/WorldTypeOverworldExtendedMixin.java
+++ src/main/java/solignomiki/times/mixin/WorldTypeOverworldExtendedMixin.java
@@ -14,40 +14,22 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import solignomiki.times.Times;
 
 @Mixin(value = WorldTypeOverworldExtended.class)
 public abstract class WorldTypeOverworldExtendedMixin extends WorldTypeOverworld {
-
-
 	public WorldTypeOverworldExtendedMixin(String languageKey, Weather defaultWeather, WindManager windManager, SeasonConfig defaultSeasonConfig) {
 		super(languageKey, defaultWeather, windManager, defaultSeasonConfig);
 	}
 
-//	@Redirect(
-//		method = "Lnet/minecraft/core/world/type/WorldTypeOverworldExtended;<init>(Ljava/lang/String;)V",
-//		//remap = false,
-//		at = @At(
-//			value = "INVOKE",
-//			target = "Lnet/minecraft/core/world/config/season/SeasonConfigBuilder;build()Lnet/minecraft/core/world/config/season/SeasonConfig;",
-//			ordinal = 0
-//		)
-//	)
-//	private SeasonConfig replaceSeasonConfigBuild(SeasonConfigBuilder builder) {
-//		System.out.println("here");
-//		return SeasonConfig.builder()
-//			.withSeasonInCycle(Seasons.OVERWORLD_SPRING, 1)
-//			.withSeasonInCycle(Seasons.OVERWORLD_SUMMER, 1)
-//			.withSeasonInCycle(Seasons.OVERWORLD_FALL, 1)
-//			.withSeasonInCycle(Seasons.OVERWORLD_WINTER, 1)
-//			.build();
-//	}
 	@Redirect(
 		method = "<init>(Ljava/lang/String;)V",
 		remap = false,
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/core/world/config/season/SeasonConfigBuilder;withSeasonInCycle(Lnet/minecraft/core/world/season/Season;I)Lnet/minecraft/core/world/config/season/SeasonConfigBuilder;")
 	)
 	private static SeasonConfigBuilder redirectFirst(SeasonConfigBuilder seasonConfigBuilder, Season season, int length) {
-		System.out.println("here");
-		return seasonConfigBuilder.withSeasonInCycle(season, 6_552);
+		// 6_552 is for 91 real life days (average of a season)
+		System.out.println(Times.config.getInt("SeasonLength"));
+		return seasonConfigBuilder.withSeasonInCycle(season, Times.config.getInt("SeasonLength"));
 	}
 }
